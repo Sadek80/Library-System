c6661ffc5f6fe92d71951cbb11064204a7933e08
Sadek80 <mohamedsadk80@gmail.com>
2023-12-23 18:15:14 +0200

    Adds StatusCodes Constants

---
+++ src/Domain/Abstractions/StatusCodes.ts
@@ -0,0 +1,13 @@
+export class StatusCodes
+{
+    static Ok = 200;
+    static Created = 201;
+    static NoContent = 204;
+
+    static BadRequest = 400;
+    static NotFound = 404;
+    static Conflict = 409;
+    static TooManyRequests = 429;
+
+    static ServerError = 500;
+}
