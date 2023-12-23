c5e20bb9528f00b1bc12003cf81f906a87104b25
Sadek80 <mohamedsadk80@gmail.com>
2023-12-23 15:57:59 +0200

    Adds BorrowersRepo Implementation

--- src/Domain/Abstractions/Repositories/IBorrowersRepository.ts
+++ src/Domain/Abstractions/Repositories/IBorrowersRepository.ts
@@ -2,6 +2,6 @@ import { IRepository } from './IRepository'
 import {Borrower} from 'Domain/Types/Borrower'
 import {BorrowerDto}  from 'Domain/Dtos/BorrowerDto'
 
-export interface IBorrowRepository extends IRepository<Borrower, BorrowerDto>
+export interface IBorrowersRepository extends IRepository<Borrower, BorrowerDto>
 {
 }
---
+++ src/Persistence/Repositories/BorrowersRepository.ts
@@ -0,0 +1,61 @@
+import { IDatabaseService } from "Domain/Abstractions/IDatabaseService";
+import { IBorrowersRepository } from "Domain/Abstractions/Repositories/IBorrowersRepository";
+import { BorrowerDto } from "Domain/Dtos/BorrowerDto";
+import { Borrower } from "Domain/Types/Borrower";
+import { Pool } from 'mysql2/promise';
+
+export class BorrowersRepository implements IBorrowersRepository{
+    private readonly _dbConnection : Pool;
+
+    constructor(private readonly _dbService : IDatabaseService)
+    {
+        this._dbConnection = _dbService.getConnection();
+    }
+    async add(borrower: Borrower): Promise<number> {
+        const [result] : any = await this._dbConnection.query(`
+            INSERT INTO borrowers (name, email, password_hash)
+            VALUES (?, ?, ?)
+            `, [borrower.name, borrower.email, borrower.password_hash]);
+
+        return result.affectedRows;    
+    }
+
+    async update(id: number, borrower: Borrower): Promise<number> {
+        const [result] : any = await this._dbConnection.query(`
+            UPDATE borrowers 
+            SET name = ?, email = ?,  password_hash = ? 
+            WHERE id = ?
+            `, [borrower.name, borrower.email, borrower.password_hash, id]);
+            
+        return result.affectedRows;       
+    }
+
+    async delete(id: number): Promise<number> {
+        const [result] : any = await this._dbConnection.query(`
+        DELETE 
+        FROM borrowers 
+        WHERE id = ?
+        `, [id]);
+        
+        return result.affectedRows;      
+    }
+
+    async get(id: number): Promise<BorrowerDto> {
+        const [result] : any = await this._dbConnection.query(`
+        SELECT name, email
+        FROM borrowers 
+        WHERE id = ?
+        `, [id]);
+
+        return result[0];    
+    }
+
+    async list(): Promise<BorrowerDto[]> {
+        const [result] : any = await this._dbConnection.query(`
+        SELECT name, email
+        FROM borrowers 
+        `);
+
+        return result;    
+    }
+}
